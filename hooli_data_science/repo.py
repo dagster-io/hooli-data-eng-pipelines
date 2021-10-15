3e1fd86e7860549a96e05c3e75f256fae9e19109
Max Gasner <max.gasner@gmail.com>
2021-10-15 16:29:25 -0700

    add schedules & sensors

--- hooli_data_science/repo.py
+++ hooli_data_science/repo.py
@@ -1,13 +1,22 @@
-from dagster import repository
+from dagster import repository, schedule_from_partitions
 
 from .jobs import dbt_prod_job, dbt_staging_job, download_prod_job, download_staging_job
+from .schedules import hourly_hn_download_schedule
+from .sensors import make_hn_tables_updated_sensor
 
 
 @repository
 def prod_repo():
-    return [dbt_prod_job, download_prod_job]
+    return [
+        make_hn_tables_updated_sensor(dbt_prod_job),
+        schedule_from_partitions(download_prod_job),
+        hourly_hn_download_schedule,
+    ]
 
 
 @repository
 def staging_repo():
-    return [dbt_staging_job, download_staging_job]
+    return [
+        make_hn_tables_updated_sensor(dbt_staging_job),
+        schedule_from_partitions(download_staging_job),
+    ]
---
+++ hooli_data_science/sensors/__init__.py
@@ -0,0 +1,1 @@
+from .hn_tables_updated_sensor import make_hn_tables_updated_sensor
