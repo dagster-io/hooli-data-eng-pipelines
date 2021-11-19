f2c7ff8dc91e893c78a463092595c20bcd0a34f7
Max Gasner <max.gasner@gmail.com>
2021-11-19 11:59:15 -0800

    Don't execute on weekends

--- hooli_data_eng/schedules/__init__.py
+++ hooli_data_eng/schedules/__init__.py
@@ -2,9 +2,15 @@ from dagster import ScheduleDefinition
 
 from ..jobs import salesforce_ingest
 
+
+def not_on_weekends(context):
+    return context.scheduled_execution_time.isoweekday() < 6
+
+
 daily_salesforce_ingest = ScheduleDefinition(
     name="daily_salesforce_ingest",
     cron_schedule="0 17 * * *",
     job=salesforce_ingest,
     description="Daily Fivetran ingest from Salesforce",
+    should_execute=not_on_weekends,
 )
