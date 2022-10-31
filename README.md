cb0b604812b57a6be0681a567b5b8aadb5c35f3c
Sean Lopp <lopp@Seans-MacBook-Pro.local>
2022-10-31 10:43:29 -0600

    updates to docs and code readability

--- README.md
+++ README.md
@@ -1,27 +1,31 @@
 # Hooli, Inc. Data Engineering
 
-Dagster application code developed by the data engineering team at Hooli.
+This repository includes Dagster application code developed by the fictional data engineering team at Hooli.
 
-Most of this example is forked from [`dagster/examples/assets_dbt_python`](https://github.com/dagster-io/dagster)
+## Getting Started
 
-A few notable changes from the quickstart example:
+You can clone and run this example locally:
 
-- Using different resources for different environments (DuckDB locally, snowflake + s3 in production)
-- Using the official DuckDB IO Manager
-- Sensors trigger a prediction job with the existing model everytime an analytics job has run, whereas running all assets creates a new model
-- The model hyper parameters are passed as job config
-- There is a partitioned model stats asset that shows how a backfill can be launched, in this case for a simplified version of model back testing 
-- The open PR shows a branch deployment with a second "competing" model that uses FB prophet. This change also highlights how you could test dependency changes.
+```
+git clone https://github.com/dagster-io/hooli-data-eng-pipelines
+pip install -e ".[dev]"
+dagit
+```
 
-TODOs:
-- Add a Spark example
-- Add a Jupyter Notebook
+## Code Structure
+
+To understand the structure, start with the file `hooli_data_eng/repository.py`. This example includes a few key Dagster concepts:
+
+- *Assets*: are used to represent the datasets the Hooli data team manages. This example includes assets generated from dbt and Python.
+- *Resources*: represent external systems. This example uses different resources for different environments (DuckDB locally, snowflake + s3 in production)
+- *Jobs*: allow us to automate when our assets are updated. This example includes jobs that run on a *Schedule* and *Sensors* that trigger jobs to run when upstream data is ready.
+- *Job Configuration* allows Dagster to parameterize tasks, this example includes a forecasting model with hyper parameters passed as job config.
+- *Partitions and Backfills* allow Dagster to represent partitioned data with no additional code. This example includes a partitioned model stats asset (`hooli_data_eng/assets/forecasting/__init__.py`). Using partitions allows the Hooli data team to track model performance overtime, account for model drift, and run back tests for new models.
 
 
-Dev Notes:
+## Deploy Architecture 
 
-- All branch deployments hit the Snowflake Database: DEMO_DB2_BRANCH
-- Prod hits the Snowflake Database: DEMO_DB2
-- Running with BRANCH or PROD locally is possible, but requires snowflake and AWS creds 
-- These creds are stored as Kubernetes secrets on the hooli-user cluster, inside of user-cloud in us-west-2
-- The code executes as the hooli-demo user, defined in the main Elementl account. The resources (incl s3 buckets) live in the user cloud sub-account
+This repository uses Dagster Cloud Hybrid architecture with GitHub Actions to provide CI/CD.
+- The main branch is deployed to Dagster Cloud using `.github/workflows/deploy.yaml`. Each commit a new Docker image is built and pushed to our container registry. These images are deployed into an EKS cluster by our running Dagster Agent which also syncronizes changes with Dagster Cloud.
+
+- The open PR in this repository shows how Dagster supports full integration testing with a *branch deployment*, in this case the PR is code for a second "competing" model. This change also highlights how you can test dependency changes. The action `.github/workflows/branch_deploy.yaml` creates the isolated branch deployment for each PR.
--- hooli_data_eng/repository.py
+++ hooli_data_eng/repository.py
@@ -30,9 +30,29 @@ from dagster._utils import file_relative_path
 # ---------------------------------------------------
 # Assets
 
+# Dagster assets specify what outputs we care about and 
+# the logical code needed to create them
+
+# Our first set of assets represent raw data, and the asset
+# definitions can be seen in /assets/raw_data/__init__.py
+# 
+# These raw datasets will be used by dbt as dbt sources
+# which can be found in dbt_project/models/sources.yml
+raw_data_assets = load_assets_from_package_module(
+    raw_data,
+    group_name="raw_data",
+    key_prefix=["raw_data"]
+)
+
+# Our second set of assets represent dbt models
+# these models are defined in the dbt_project
+# folder
+# 
+# The dbt file dbt_project/config/profiles.yaml 
+# specifies what databases to targets, and locally will 
+# execute against a DuckDB
 DBT_PROJECT_DIR = file_relative_path(__file__, "../dbt_project")
 DBT_PROFILES_DIR = file_relative_path(__file__, "../dbt_project/config")
-
 
 dbt_assets = load_assets_from_dbt_project(
     DBT_PROJECT_DIR,
@@ -40,12 +60,10 @@ dbt_assets = load_assets_from_dbt_project(
     key_prefix=["analytics"]
 )
 
-raw_data_assets = load_assets_from_package_module(
-    raw_data,
-    group_name="raw_data",
-    key_prefix=["raw_data"]
-)
 
+# Our final set of assets represent Python code that
+# should run after dbt. These assets are defined in 
+# assets/forecasting/__init__.py
 forecasting_assets = load_assets_from_package_module(
     forecasting,
     group_name="forecasting"
@@ -55,6 +73,20 @@ forecasting_assets = load_assets_from_package_module(
 # ---------------------------------------------------
 # Resources
 
+# Resources represent external systems and, and specifically IO Managers
+# tell dagster where our assets should be materialized. In dagster 
+# resources are separate from logical code to make it possible
+# to develop locally, run tests, and run integration tests
+# 
+# This project is designed for everything to run locally 
+# using the file system and DuckDB as the primary development resources
+#
+# PRs use a "branch" environment that mirrors production with 
+# staging Snowflake and S3 resources
+#
+# The production deployment on Dagster Cloud uses production Snowflake
+# and S3 resources
+
 def get_env():
     if os.getenv("DAGSTER_CLOUD_IS_BRANCH_DEPLOYMENT", "") == "1":
         return "BRANCH"
@@ -62,17 +94,18 @@ def get_env():
         return "PROD"
     return "LOCAL"
 
+# Locally, have dagster use DuckDB as the database
+# See dbt_project/config/profiles.yml to see the matching dbt config
 duckdb_io_manager = build_duckdb_io_manager([DuckDBPandasTypeHandler()]).configured(
     {"duckdb_path": os.path.join(DBT_PROJECT_DIR, "example.duckdb")}
 )
 
+# For production we'll use snowflake and s3
 snowflake_io_manager = build_snowflake_io_manager([SnowflakePandasTypeHandler()])
+s3 = s3_resource.configured({ "region_name": "us-west-2"})
 
-s3 = s3_resource.configured({
-            "region_name": "us-west-2"
-})
-
-
+# Similar to having different dbt targets, here we create the resource
+# configuration by environment
 resource_def = {
     "LOCAL": {
         "io_manager": duckdb_io_manager,
@@ -114,7 +147,7 @@ resource_def = {
             "warehouse": "TINY_WAREHOUSE"
         }),
         "model_io_manager": s3_pickle_io_manager.configured({
-            "s3_bucket": "hooli-demo-branch"
+            "s3_bucket": "hooli-demo"
         }),
         "s3": s3,
         "dbt": dbt_cli_resource.configured({
@@ -134,20 +167,34 @@ assets_with_resources = with_resources(
 # ---------------------------------------------------
 # Jobs and Sensors
 
+# With assets defined we have everything to run Dagster
+# ourselves if we wanted to manually create assets. 
+# Most of the time you will want to automate asset creation.
+# In dagster, jobs allow you to update all or some assets.
+# Jobs can be run on a schedule, or in response to an external 
+# event using a sensor.
+
+# This job updates all of the assets upstream of "daily_order_summary",
+# which is an asset representing a model in dbt. In this case, 
+# this job will update the raw data assets and then the dbt models
+# upstream of daily_order_summary.
 analytics_job = define_asset_job("refresh_analytics_model_job",
      selection=AssetSelection.keys(["analytics", "daily_order_summary"]).upstream(), 
-    #  config = {
-    #     "resources": resource_def[get_env()]
-    #  }
 )
 
+# This schedule tells dagster to run the analytics_job daily
+analytics_schedule = ScheduleDefinition(job=analytics_job, cron_schedule="0 * * * *")
+
+# This job selects the predicted_orders asset defined in 
+# assets/forecasting/__init__.py
 predict_job = define_asset_job("predict_job", 
     selection=AssetSelection.keys(["forecasting","predicted_orders"]),
-    # config = {
-    #     "resources": resource_def[get_env()]
-    #  }
 )
 
+# This sensor listens for changes to the orders_augmented asset which 
+# represents a dbt model. When the table managed by dbt is updated, 
+# this sensor will trigger the predict_job above, ensuring that anytime 
+# new order data is produced the forecast is updated
 @asset_sensor(asset_key=AssetKey(["analytics", "orders_augmented"]), job = predict_job)
 def orders_sensor(context: SensorEvaluationContext, asset_event: EventLogEntry):
     yield RunRequest(
@@ -158,12 +205,9 @@ def orders_sensor(context: SensorEvaluationContext, asset_event: EventLogEntry):
 # ---------------------------------------------------
 # Repository
 
+# A repository is the collection of assets, jobs, schedules, and sensors.
+# Dagster Cloud deployments can contain mulitple repositories.
+
 @repository
 def hooli_data_eng():
-    return assets_with_resources + [
-        ScheduleDefinition(job=analytics_job, cron_schedule="0 * * * *")
-    ] + [ 
-    orders_sensor 
-    ] + [
-        analytics_job, predict_job
-    ]
+    return assets_with_resources + [analytics_schedule] + [orders_sensor] + [analytics_job, predict_job]
