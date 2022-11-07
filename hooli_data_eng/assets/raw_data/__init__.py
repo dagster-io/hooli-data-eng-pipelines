17d577e51ea7eb12f193576a2e286020777a0510
Sean Lopp <lopp@Seans-MacBook-Pro.local>
2022-11-07 12:51:34 -0700

    refactor random data to api calls

--- hooli_data_eng/assets/raw_data/__init__.py
+++ hooli_data_eng/assets/raw_data/__init__.py
@@ -1,28 +1,27 @@
-import numpy as np
 import pandas as pd
-from hooli_data_eng.utils import random_data
-
 from dagster import asset
 
 
-@asset(compute_kind="random")
-def users() -> pd.DataFrame:
+@asset(
+    compute_kind="api",
+    required_resource_keys={"data_api"}
+)
+def users(context) -> pd.DataFrame:
     """A table containing all users data"""
-    return pd.DataFrame(
-        {
-            "user_id": range(1000),
-            "company": np.random.choice(
-                ["FoodCo", "ShopMart", "SportTime", "FamilyLtd"], size=1000
-            ),
-            "is_test_user": np.random.choice([True, False], p=[0.002, 0.998], size=1000),
-        }
-    )
+    api = context.resources.data_api
+    resp = api.get_users()
+    users = pd.read_json(resp.json())
+    return users
 
 
-@asset(compute_kind="random")
-def orders() -> pd.DataFrame:
+@asset(
+    compute_kind="api",
+    required_resource_keys={"data_api"}
+)
+def orders(context) -> pd.DataFrame:
     """A table containing all orders that have been placed"""
-    return random_data(
-        extra_columns={"order_id": str, "quantity": int, "purchase_price": float, "sku": str},
-        n=10000,
-    )
+    api = context.resources.data_api
+    resp = api.get_orders()
+    orders = pd.read_json(resp.json())
+    orders['dt'] = pd.to_datetime(orders['dt'], unit = "ms")
+    return orders
