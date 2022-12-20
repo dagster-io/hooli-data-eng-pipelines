5020ca56f8bdedf8900189e98710ec252a7b576e
Sean Lopp <lopp@elementl.com>
2022-12-20 13:09:23 -0700

    more logos

--- hooli_data_eng/assets/marketing/__init__.py
+++ hooli_data_eng/assets/marketing/__init__.py
@@ -19,19 +19,9 @@ def avg_order(company_perf: pd.DataFrame) -> pd.DataFrame:
 @asset(
     key_prefix="MARKETING", 
     freshness_policy=FreshnessPolicy(maximum_lag_minutes=90), 
-    compute_kind="pandas"
+    compute_kind="snowflake"
 )
 def max_order(company_perf: pd.DataFrame) -> pd.DataFrame:
     return pd.DataFrame({
         "max_order": [max(company_perf['n_orders'])]
     })
-
-# @asset(
-#     key_prefix="MARKETING", 
-#     freshness_policy=FreshnessPolicy(maximum_lag_minutes=90), 
-#     compute_kind="pandas"
-# )
-# def min_order(company_perf: pd.DataFrame) -> pd.DataFrame:
-#     return pd.DataFrame({
-#         "min_order": [min(company_perf['n_orders'])]
-#     })
