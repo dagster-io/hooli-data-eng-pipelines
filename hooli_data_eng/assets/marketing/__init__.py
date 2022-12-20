0e303bc81581ce37eefb09f1370e3bc67c967a77
Sean Lopp <lopp@elementl.com>
2022-12-20 13:27:21 -0700

    add marketing kpi asset

--- hooli_data_eng/assets/marketing/__init__.py
+++ hooli_data_eng/assets/marketing/__init__.py
@@ -1,4 +1,4 @@
-from dagster import asset, FreshnessPolicy
+from dagster import asset, FreshnessPolicy, AssetIn
 import pandas as pd
 
 # These assets take data from a SQL table managed by 
@@ -24,4 +24,15 @@ def avg_order(company_perf: pd.DataFrame) -> pd.DataFrame:
 def max_order(company_perf: pd.DataFrame) -> pd.DataFrame:
     return pd.DataFrame({
         "max_order": [max(company_perf['n_orders'])]
+    })
+
+
+@asset(
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=30), 
+    compute_kind="snowflake",
+    ins={"daily_order_summary": AssetIn(key_prefix=["ANALYTICS"])},
+)
+def daily_avg_order(daily_order_summary: pd.DataFrame):
+    return pd.DataFrame({
+        "count_per_day": [sum(daily_order_summary['num_orders']) / len(daily_order_summary)]
     })
