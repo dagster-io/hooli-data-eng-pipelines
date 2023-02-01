1e582955a7a52b48f343c8ec8284242a55060159
Sean Lopp <lopp@elementl.com>
2023-02-01 15:49:11 -0700

    move all reqs to requirements.txt and attempt install of master for dagster stuff

--- setup.py
+++ setup.py
@@ -6,16 +6,6 @@ if __name__ == "__main__":
         packages=find_packages(exclude=["hooli_data_eng_tests"]),
         package_data={"hooli_data_eng": ["dbt_project/*"]},
         install_requires=[
-            "pandas",
-            "numpy",
-            "scipy",
-            "dbt-core",
-            "dbt-duckdb",
-            "dbt-snowflake", 
-            "dagster-cloud",
-            "plotnine",
-            "responses", 
-            "requests"
         ],
         extras_require={"dev": ["dagit", "pytest"]},
     )
