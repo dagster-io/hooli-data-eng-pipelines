0e8edb34f9657d14cb263ffccb66246f8c1915c2
benpankow <ben@elementl.com>
2023-03-30 15:08:03 -0700

    rebase to pick up new partitions stuff

--- hooli_data_eng/resources/sensor_smtp.py
+++ hooli_data_eng/resources/sensor_smtp.py
@@ -1,16 +1,12 @@
-
 from dagster import FreshnessPolicySensorContext, ConfigurableResource
-from dagster._utils.alert import (
-    EMAIL_MESSAGE,
-    send_email_via_ssl
-)
+from dagster._utils.alert import EMAIL_MESSAGE, send_email_via_ssl
 from dagster._core.errors import DagsterInvalidDefinitionError
 from typing import Callable, Optional, Sequence
-import datetime 
+import datetime
 import ssl
 import smtplib
 import os
-
+from typing import List
 
 
 def send_email_via_starttls(
@@ -28,16 +24,19 @@ def send_email_via_starttls(
         server.login(email_username, email_password)
         server.sendmail(email_from, email_to, message)
 
+
 def _default_delay_email_body(context) -> str:
     return "<br>".join(
         [
             f"Asset { context.asset_key } late!",
-            f"Late by: { context.minutes_late } minutes"
+            f"Late by: { context.minutes_late } minutes",
         ]
     )
 
+
 def _default_delay_email_subject(context) -> str:
-    return f"Dagster Asset Late: { context.asset_key }" 
+    return f"Dagster Asset Late: { context.asset_key }"
+
 
 def send_email_alert(
     context: FreshnessPolicySensorContext,
@@ -45,8 +44,12 @@ def send_email_alert(
     email_username: str,
     email_password: str,
     email_to: Sequence[str],
-    email_body_fn: Callable[["FreshnessPolicySensorContext"], str] = _default_delay_email_body,
-    email_subject_fn: Callable[["FreshnessPolicySensorContext"], str] = _default_delay_email_subject,
+    email_body_fn: Callable[
+        ["FreshnessPolicySensorContext"], str
+    ] = _default_delay_email_body,
+    email_subject_fn: Callable[
+        ["FreshnessPolicySensorContext"], str
+    ] = _default_delay_email_subject,
     smtp_host: str = "smtp.gmail.com",
     smtp_type: str = "SSL",
     smtp_port: Optional[int] = None,
@@ -62,41 +65,55 @@ def send_email_alert(
 
     if smtp_type == "SSL":
         send_email_via_ssl(
-            email_from, email_password, email_to, message, smtp_host, smtp_port=smtp_port or 465
+            email_from,
+            email_password,
+            email_to,
+            message,
+            smtp_host,
+            smtp_port=smtp_port or 465,
         )
     elif smtp_type == "STARTTLS":
         send_email_via_starttls(
-            email_from, email_password, email_username, email_to, message, smtp_host, smtp_port=smtp_port or 587
+            email_from,
+            email_password,
+            email_username,
+            email_to,
+            message,
+            smtp_host,
+            smtp_port=smtp_port or 587,
         )
     else:
-        raise DagsterInvalidDefinitionError(f'smtp_type "{smtp_type}" is not supported.')
+        raise DagsterInvalidDefinitionError(
+            f'smtp_type "{smtp_type}" is not supported.'
+        )
 
 
 class SESEmailAlert(ConfigurableResource):
     smtp_host: str
-    smtp_username: str 
-    smtp_password: str  
-    smtp_email_from: str 
-    smtp_email_to: list[str]
+    smtp_username: str
+    smtp_password: str
+    smtp_email_from: str
+    smtp_email_to: List[str]
 
     def send_email_alert(self, context):
-        
+
         return send_email_alert(
-            context = context,
-            email_username= self.smtp_username,
-            email_from = self.smtp_email_from,
-            email_to= self.smtp_email_to,
-            email_password= self.smtp_password,
-            smtp_host= self.smtp_host,
+            context=context,
+            email_username=self.smtp_username,
+            email_from=self.smtp_email_from,
+            email_to=self.smtp_email_to,
+            email_password=self.smtp_password,
+            smtp_host=self.smtp_host,
             smtp_type="STARTTLS",
-            smtp_port=587
+            smtp_port=587,
         )
 
-class LocalEmailAlert(ConfigurableResource):
-    smtp_email_from: str  
-    smtp_email_to: list[str]
 
-    def send_email_alert(self,context):
+class LocalEmailAlert(ConfigurableResource):
+    smtp_email_from: str
+    smtp_email_to: List[str]
+
+    def send_email_alert(self, context):
         email_body = _default_delay_email_body(context)
         message = EMAIL_MESSAGE.format(
             email_to=",".join(self.smtp_email_to),
@@ -106,4 +123,4 @@ class LocalEmailAlert(ConfigurableResource):
             randomness=datetime.datetime.now(),
         )
         print(message)
-        return 
+        return
