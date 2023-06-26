34ab731445fba9f2cf2f92973665159e9705f319
Sean Lopp <lopp@elementl.com>
2023-06-26 14:58:24 -0600

    Delete dbt_project/target directory

--- dbt_project/target/compiled/dbt_project/models/cleaned/users_cleaned.sql
+++
@@ -1,5 +0,0 @@
-select
-        user_id,
-        company
-from DEMO_DB2.raw_data.users
-where not is_test_user
--- dbt_project/target/compiled/dbt_project/models/marketing/company_perf.sql
+++
@@ -1,6 +0,0 @@
-select
-        company,
-        sum(n_orders) as n_orders,
-        sum(total_revenue) as total_revenue
-from DEMO_DB2.analytics.company_stats
-group by 1
