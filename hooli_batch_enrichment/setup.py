e27ec167dad638e8e83d6bcf5e1d3216499b82ed
Sean Lopp <lopp@elementl.com>
2023-10-18 12:48:53 -0600

    Lopp add more frequency (#44)
    * add another project with more frequent job runs and example of dynamicout
    
    * update workflow
    
    * updated failed workflow
    
    * another attempt
    
    * update name and frequency
    
    * adjust flakiness and retry policies

---
+++ hooli_batch_enrichment/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dagster_batch_enrichment",
+    packages=find_packages(),
+    install_requires=[
+        "dagster",
+        "dagster-duckdb",
+        "pandas",
+        "responses",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagit", "pytest"]},
+)
