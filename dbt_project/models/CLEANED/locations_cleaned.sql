c2adcf16b73cc44053874ec6dc20fd1f7bb9ee7f
izzy <izzy@dagsterlabs.com>
2023-12-11 22:43:04 -0700

    updated AssetKey to location

--- dbt_project/models/CLEANED/locations_cleaned.sql
+++ dbt_project/models/CLEANED/locations_cleaned.sql
@@ -1,7 +1,7 @@
 with source as (
     
 select *
-from {{ source("RAW_DATA", "raw_location") }}
+from {{ source("RAW_DATA", "location") }}
 
 ),
 
--- hooli-demo-assets/hooli_demo_assets/jobs/__init__.py
+++ hooli-demo-assets/hooli_demo_assets/jobs/__init__.py
@@ -1,6 +1,6 @@
 from dagster import AssetSelection, define_asset_job
 
-raw_location_by_day = AssetSelection.keys(["RAW_DATA", "raw_location"])
+raw_location_by_day = AssetSelection.keys(["RAW_DATA", "location"])
 
 daily_sling_job = define_asset_job(
     name="daily_sling_job",
