c34a83acd68b848c0400d5be5eb5d31973686af5
izzy <izzy@dagsterlabs.com>
2024-03-07 11:44:27 -0700

    shows compiled sql for dbt models in the event logs

--- hooli_data_eng/assets/dbt_assets.py
+++ hooli_data_eng/assets/dbt_assets.py
@@ -125,9 +125,18 @@ def _process_partitioned_dbt_assets(context: OpExecutionContext, dbt: DbtCliReso
     dbt_vars = {"min_date": str(first_partition), "max_date": str(last_partition)}
     dbt_args = ["build", "--vars", json.dumps(dbt_vars)]
 
+    # Invoke dbt CLI
     dbt_cli_task = dbt.cli(dbt_args, context=context)
 
+    # Emits an AssetObservation for each asset materialization, which is used to 
+    # identify the Snowflake credit consumption
     yield from dbt_with_snowflake_insights(context, dbt_cli_task)
+
+    # fetch run_results.json to log compiled SQL
+    run_results_json = dbt_cli_task.get_artifact("run_results.json")
+    for result in run_results_json["results"]:
+        model_name = result.get("unique_id")
+        context.log.info(f"Compiled SQL for {model_name}:\n{result['compiled_code']}")
 
 
 @dbt_assets(
