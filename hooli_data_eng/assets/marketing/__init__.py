33c8b45e77d61c4ea9dc688728fef0761d461654
Sean Lopp <lopp@elementl.com>
2024-04-16 12:31:37 -0600

    remove freshness policies in favor of freshness checks (#81)
    * remove first freshness policy
    
    * add anomaly check too

--- hooli_data_eng/assets/marketing/__init__.py
+++ hooli_data_eng/assets/marketing/__init__.py
@@ -1,84 +1,110 @@
 from dagster import (
     asset,
-    FreshnessPolicy,
+    build_last_update_freshness_checks,
+    build_sensor_for_freshness_checks,
     AssetIn,
     DynamicPartitionsDefinition,
     MetadataValue,
-    AutoMaterializePolicy,
     AssetExecutionContext,
     AssetCheckResult,
-    asset_check
+    asset_check,
+    FreshnessPolicy,
+    define_asset_job, 
+    ScheduleDefinition,
+    AssetSelection
 )
 import pandas as pd
+import datetime
+from dagster_cloud.anomaly_detection import build_anomaly_detection_freshness_checks
 
 from hooli_data_eng.assets.dbt_assets import allow_outdated_parents_policy
 
-# These assets take data from a SQL table managed by 
-# dbt and create summaries using pandas 
-# The assets are updated via freshness policies 
-# and an associated reconciliation sensor
+
+# These assets take data from a SQL table managed by
+# dbt and create summaries using pandas
 @asset(
-    key_prefix="MARKETING", 
-    freshness_policy=FreshnessPolicy(maximum_lag_minutes=24*60), 
+    key_prefix="MARKETING",
     auto_materialize_policy=allow_outdated_parents_policy,
     compute_kind="pandas",
     owners=["team:programmers", "lopp@dagsterlabs.com"],
-    ins={"company_perf": AssetIn(key_prefix=["ANALYTICS"])}
+    ins={"company_perf": AssetIn(key_prefix=["ANALYTICS"])},
 )
-def avg_orders(context: AssetExecutionContext, company_perf: pd.DataFrame) -> pd.DataFrame:
-    """ Computes avg order KPI, must be updated regularly for exec dashboard """
+def avg_orders(
+    context: AssetExecutionContext, company_perf: pd.DataFrame
+) -> pd.DataFrame:
+    """Computes avg order KPI, must be updated regularly for exec dashboard"""
 
-    return pd.DataFrame({
-        "avg_order": company_perf['total_revenue'] / company_perf['n_orders'] 
-    })
-
-@asset_check(
-        description="check that avg orders are expected",
-        asset=avg_orders
-)
-def check_avg_orders(context, avg_orders: pd.DataFrame):
-    avg = avg_orders['avg_order'][0]
-    return AssetCheckResult(
-        passed= True if (avg < 50) else False, 
-        metadata={"actual average": avg, "threshold": 50}
+    return pd.DataFrame(
+        {"avg_order": company_perf["total_revenue"] / company_perf["n_orders"]}
     )
 
+
+@asset_check(description="check that avg orders are expected", asset=avg_orders)
+def check_avg_orders(context, avg_orders: pd.DataFrame):
+    avg = avg_orders["avg_order"][0]
+    return AssetCheckResult(
+        passed=True if (avg < 50) else False,
+        metadata={"actual average": avg, "threshold": 50},
+    )
+
+
 @asset(
-    key_prefix="MARKETING", 
-    freshness_policy=FreshnessPolicy(maximum_lag_minutes=24*60), 
-    compute_kind="snowflake", 
+    key_prefix="MARKETING",
+    compute_kind="snowflake",
     owners=["team:programmers"],
-    ins={"company_perf": AssetIn(key_prefix=["ANALYTICS"])}
+    ins={"company_perf": AssetIn(key_prefix=["ANALYTICS"])},
 )
 def min_order(context, company_perf: pd.DataFrame) -> pd.DataFrame:
-    """ Computes min order KPI """
-    min_order = min(company_perf['n_orders'])
+    """Computes min order KPI"""
+    min_order = min(company_perf["n_orders"])
 
-    context.add_output_metadata({
-        "min_order": min_order
-    })
+    context.add_output_metadata({"min_order": min_order})
 
-    return pd.DataFrame({
-        "min_order": [min_order]
-    })
+    return pd.DataFrame({"min_order": [min_order]})
 
 
 product_skus = DynamicPartitionsDefinition(name="product_skus")
+
 
 @asset(
     partitions_def=product_skus,
     io_manager_key="model_io_manager",
     compute_kind="hex",
     key_prefix="MARKETING",
-    ins={"sku_stats": AssetIn(key_prefix=["ANALYTICS"])}
+    ins={"sku_stats": AssetIn(key_prefix=["ANALYTICS"])},
 )
 def key_product_deepdive(context, sku_stats):
-    """ Creates a file for a BI tool based on the current quarters top product, represented as a dynamic partition """
+    """Creates a file for a BI tool based on the current quarters top product, represented as a dynamic partition"""
     key_sku = context.partition_key
-    sku = sku_stats[sku_stats['sku'] == key_sku]
+    sku = sku_stats[sku_stats["sku"] == key_sku]
     context.add_output_metadata(
-        {
-            "sku_preview": MetadataValue.md(sku.head().to_markdown())
-        }
+        {"sku_preview": MetadataValue.md(sku.head().to_markdown())}
     )
     return sku
+
+
+min_order_freshness_check = build_last_update_freshness_checks(
+    assets=[min_order],
+    lower_bound_delta=datetime.timedelta(
+        hours=24
+    ),  # expect new data at least once a day
+)
+
+avg_orders_freshness_check = build_anomaly_detection_freshness_checks(
+    assets=[avg_orders], 
+    params=None
+)
+
+min_order_freshness_check_sensor = build_sensor_for_freshness_checks(
+    freshness_checks=[min_order_freshness_check], 
+    minimum_interval_seconds=10*60
+)
+
+avg_orders_freshness_check_schedule = ScheduleDefinition(
+    name="check_avg_orders_freshness_schdule",
+    cron_schedule="*/10 * * * *",
+    job=define_asset_job(
+        "check_avg_orders_freshness_job",
+        selection=AssetSelection.checks(avg_orders_freshness_check)
+    )
+)
