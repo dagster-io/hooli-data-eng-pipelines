1d87fddc9d261babc4352863a04b4264dcea961c
Christian Minich <indyman402@gmail.com>
2024-04-24 14:18:49 -0400

    remove dbt assets from graph

--- hooli_data_eng/definitions.py
+++ hooli_data_eng/definitions.py
@@ -10,9 +10,9 @@ from hooli_data_eng.assets import forecasting, raw_data, marketing, dbt_assets
 from hooli_data_eng.assets.dbt_assets import dbt_slim_ci_job
 from hooli_data_eng.assets.marketing import check_avg_orders
 from hooli_data_eng.assets.raw_data import check_users
-from hooli_data_eng.jobs import analytics_job, predict_job
+#from hooli_data_eng.jobs import analytics_job, predict_job
 from hooli_data_eng.resources import get_env, resource_def
-from hooli_data_eng.schedules import analytics_schedule
+#from hooli_data_eng.schedules import analytics_schedule
 from hooli_data_eng.sensors import orders_sensor
 from hooli_data_eng.sensors.watch_s3 import watch_s3_sensor
 from hooli_data_eng.assets.marketing import avg_orders_freshness_check, min_order_freshness_check, min_order_freshness_check_sensor, check_avg_orders, avg_orders_freshness_check_schedule
@@ -45,11 +45,11 @@ dbt_assets = load_assets_from_modules([dbt_assets])
 # Our final set of assets represent Python code that
 # should run after dbt. These assets are defined in
 # assets/forecasting/__init__.py
-forecasting_assets = load_assets_from_package_module(
-    forecasting, group_name="FORECASTING"
-)
+# forecasting_assets = load_assets_from_package_module(
+#     forecasting, group_name="FORECASTING"
+# )
 
-marketing_assets = load_assets_from_package_module(marketing, group_name="MARKETING")
+# marketing_assets = load_assets_from_package_module(marketing, group_name="MARKETING")
 
 # ---------------------------------------------------
 # Definitions
@@ -61,15 +61,15 @@ defs = Definitions(
     executor=multiprocess_executor.configured(
         {"max_concurrent": 3}
     ),  
-    assets=[*dbt_assets, *raw_data_assets, *forecasting_assets, *marketing_assets],
-    asset_checks=[check_users, check_avg_orders, avg_orders_freshness_check, min_order_freshness_check],
+    assets=[*raw_data_assets], #,,*forecasting_assets, *marketing_assets
+    asset_checks=[check_users], #, check_avg_orders, avg_orders_freshness_check, min_order_freshness_check],
     resources=resource_def[get_env()],
-    schedules=[analytics_schedule, avg_orders_freshness_check_schedule],
+   # schedules=[analytics_schedule],# avg_orders_freshness_check_schedule],
     sensors=[
-       orders_sensor,   
+       #orders_sensor,   
        watch_s3_sensor,
 #       asset_delay_alert_sensor,
-       min_order_freshness_check_sensor
-    ],
-    jobs=[analytics_job, predict_job, dbt_slim_ci_job],
+      # min_order_freshness_check_sensor
+    ]#,
+    #jobs=[analytics_job, predict_job, dbt_slim_ci_job],
 )
