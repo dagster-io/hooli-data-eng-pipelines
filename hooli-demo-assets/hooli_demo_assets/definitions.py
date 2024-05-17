fd6c25f1eea5dfcb8ef2e1a58b5dabb7a964cc40
izzy <izzy@dagsterlabs.com>
2024-05-16 18:23:19 -0600

    updated the definitions to use my_sling_assets

--- hooli-demo-assets/hooli_demo_assets/definitions.py
+++ hooli-demo-assets/hooli_demo_assets/definitions.py
@@ -1,20 +1,18 @@
 from dagster import (
-    Definitions,
-    load_assets_from_modules,
-    
+   Definitions,  
 )
 
-from hooli_demo_assets.assets import sling
+from hooli_demo_assets.assets.sling import my_sling_assets
 from hooli_demo_assets.jobs import daily_sling_job
-from hooli_demo_assets.resources import get_env, resource_def
-from hooli_demo_assets.schedules import daily_sling_assets 
+from hooli_demo_assets.resources import sling_resource
+from hooli_demo_assets.schedules import daily_sling_assets
 
-
-sling_assets = load_assets_from_modules([sling], key_prefix="RAW_DATA", group_name="RAW_DATA")
 
 defs = Definitions(
-    assets=[*sling_assets],
-    schedules=[daily_sling_assets],
-    jobs=[daily_sling_job],
-    resources=resource_def[get_env()],
+   assets=[my_sling_assets],
+   schedules=[daily_sling_assets],
+   jobs=[daily_sling_job],
+   resources={
+       "sling": sling_resource
+   },
 )
--- hooli-demo-assets/hooli_demo_assets/jobs/__init__.py
+++ hooli-demo-assets/hooli_demo_assets/jobs/__init__.py
@@ -1,8 +1,10 @@
 from dagster import AssetSelection, define_asset_job
 
+
 raw_location_by_day = AssetSelection.keys(["RAW_DATA", "locations"])
 
+
 daily_sling_job = define_asset_job(
-    name="daily_sling_job",
-    selection=raw_location_by_day,
+   name="daily_sling_job",
+   selection=raw_location_by_day,
 )
