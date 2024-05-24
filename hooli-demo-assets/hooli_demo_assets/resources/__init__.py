3024396e28bc0bde640771e4110d36bf5562c43c
izzy <60406698+izzye84@users.noreply.github.com>
2024-05-24 09:11:48 -0600

    Update hooli-demo-assets/hooli_demo_assets/resources/__init__.py
    Adding database param to be consistent with other environments—bug still exists though.
    
    Co-authored-by: Christian Minich <indyman402@gmail.com>

--- hooli-demo-assets/hooli_demo_assets/resources/__init__.py
+++ hooli-demo-assets/hooli_demo_assets/resources/__init__.py
@@ -67,6 +67,7 @@ def create_sling_resource(env: str):
            name="DUCKDB",
            type="duckdb",
            instance=f"{DUCKDB_PATH}",
+           database="example",
            schema="RAW_DATA",
        ))
    elif env == 'BRANCH':
