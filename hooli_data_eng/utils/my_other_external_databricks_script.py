619b18e03cc8c12d4616a5a13efb732748282f9c
izzy <izzy@dagsterlabs.com>
2024-08-16 16:18:45 -0600

    fixed bug with hooli_basics and added an additional databricks pipes example

--- hooli_data_eng/utils/my_other_external_databricks_script.py
+++ hooli_data_eng/utils/my_other_external_databricks_script.py
@@ -1,22 +1,16 @@
-from pathlib import Path
+import random
 
-from dagster import (
-    AnchorBasedFilePathMapping,
-    asset,
-    asset_check,
-    AssetCheckResult,
-    Definitions,
-    with_source_code_references,
-)
-from dagster._core.definitions.tags import StorageKindTagSet
-from dagster_cloud.metadata.source_code import link_code_references_to_git_if_cloud
 from pandas import DataFrame, read_html, get_dummies, to_numeric
 from sklearn.linear_model import LinearRegression as Regression
+from sklearn.metrics import mean_squared_error
 
-@asset(
-    compute_kind="Kubernetes",
-    tags={**StorageKindTagSet(storage_kind="S3")},
+from dagster_pipes import (
+    PipesDbfsContextLoader,
+    PipesDbfsMessageWriter,
+    open_dagster_pipes,
 )
+
+
 def country_stats() -> DataFrame:
     df = read_html("https://tinyurl.com/mry64ebh", flavor='html5lib')[0]
     df.columns = ["country", "Population (1 July 2022)", "Population (1 July 2023)", "Change", "UN Continental Region[1]", "UN Statistical Subregion[1]"]
@@ -31,38 +25,40 @@ def country_stats() -> DataFrame:
     df["pop_change"] = ((to_numeric(df["pop_2023"]) / to_numeric(df["pop_2022"])) - 1)*100
     return df
 
-@asset_check(
-       asset=country_stats 
-)
-def check_country_stats(country_stats):
-    return AssetCheckResult(passed=True)
 
-@asset(
-    compute_kind="Kubernetes",
-    tags={**StorageKindTagSet(storage_kind="S3")},
-)
 def change_model(country_stats: DataFrame) -> Regression:
     data = country_stats.dropna(subset=["pop_change"])
     dummies = get_dummies(data[["continent"]])
     return Regression().fit(dummies, data["pop_change"])
 
-@asset(
-    compute_kind="Kubernetes",
-    tags={**StorageKindTagSet(storage_kind="S3")},
-)
+
 def continent_stats(country_stats: DataFrame, change_model: Regression) -> DataFrame:
     result = country_stats.groupby("continent").sum()
     result["pop_change_factor"] = change_model.coef_
     return result
 
-defs = Definitions(
-    assets=link_code_references_to_git_if_cloud(
-        with_source_code_references([country_stats, continent_stats, change_model]),
-        file_path_mapping=AnchorBasedFilePathMapping(
-            local_file_anchor=Path(__file__),
-            file_anchor_path_in_repository="hooli_basics/definitions.py",
-        ),
-    ), 
-    asset_checks=[check_country_stats]
-)
+if __name__ == "__main__":
 
+    with open_dagster_pipes(
+        context_loader=PipesDbfsContextLoader(),
+        message_writer=PipesDbfsMessageWriter(),
+    ) as pipes:
+        
+        # Generate country statistics
+        country_data = country_stats()
+
+        # Build a regression model to predict population change based on continent
+        model = change_model(country_data)
+
+        # Calculate statistics by continent and add the population change factor
+        stats_by_continent = continent_stats(country_data, model)
+
+        pipes.report_asset_materialization(
+            asset_key="my_databricks_asset",
+            metadata={
+                "num_records": len(stats_by_continent),
+                "columns": list(stats_by_continent.columns),
+                "mean_squared_error": mean_squared_error(stats_by_continent["pop_change"], stats_by_continent["pop_change_factor"]),
+                "r2_score": random.choice([0.72, 0.54, 0.33, 0.37, 0.75]),
+                },
+        )
