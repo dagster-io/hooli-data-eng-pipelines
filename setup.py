25a33198d8073f5797f2da818415631526a9dded
Christian Minich <indyman402@gmail.com>
2024-10-31 14:26:56 -0400

    remove pin

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
             "requests_toolbelt",
             "html5lib",
             "scikit-learn",
-            "dagster-powerbi==0.0.11"
+            "dagster-powerbi"
         ],
         extras_require={"dev": ["dagster-webserver", "pytest"],
                         "sling": ["dagster-embedded-elt"]},
