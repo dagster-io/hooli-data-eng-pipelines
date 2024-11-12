4e2089146a2998659f452efab5c41582b93d0fde
Christian Minich <indyman402@gmail.com>
2024-11-12 16:06:53 -0500

    make a change to a dbt model

--- dbt_project/models/CLEANED/orders_cleaned.sql
+++ dbt_project/models/CLEANED/orders_cleaned.sql
@@ -3,6 +3,7 @@ select
         user_id,
         quantity,
         purchase_price,
+        purchase_price as purchase_price_2,
         sku,
         dt,
         cast(dt as datetime) as order_date,
