385d1b6b29824edf53265fe446aabf618b98ee67
Christian Minich <indyman402@gmail.com>
2025-05-28 14:09:05 -0400

    fix path for batch enrichment

--- hooli-batch-enrichment/hooli_batch_enrichment/definitions.py
+++ hooli-batch-enrichment/hooli_batch_enrichment/definitions.py
@@ -13,7 +13,6 @@ from hooli_batch_enrichment.warehouse import MyWarehouse
 from hooli_batch_enrichment.assets import raw_data, enriched_data
 from dagster_cloud.metadata.source_code import link_code_references_to_git_if_cloud
 
-
 # define a job and schedule to run the pipeline
 # alternatively could use freshness policies and auto-materialization, partitions, or other ways to orient the schedule
 run_assets_job = define_asset_job(
@@ -31,7 +30,7 @@ defs = Definitions(
         with_source_code_references([raw_data, enriched_data]),
         file_path_mapping=AnchorBasedFilePathMapping(
             local_file_anchor=Path(__file__),
-            file_anchor_path_in_repository="hooli_batch_enrichment/hooli_batch_enrichment/definitions.py",
+            file_anchor_path_in_repository="hooli-batch-enrichment/hooli_batch_enrichment/definitions.py",
         ),
     ),
     schedules=[run_assets_30min],
@@ -39,6 +38,6 @@ defs = Definitions(
     resources={
         "api": EnrichmentAPI(),
         # place holder for a real warehouse with required connection config
-        "warehouse": MyWarehouse(path="raw_data.csv"),
+        "warehouse": MyWarehouse(path=str(Path(__file__).parent.joinpath("raw_data.csv"))),
     },
 )
