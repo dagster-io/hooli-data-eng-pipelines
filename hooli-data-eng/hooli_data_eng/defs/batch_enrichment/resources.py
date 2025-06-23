cb4a70a0520bc283826e2b4a380e29d407e0dd16
benpankow <ben@dagsterlabs.com>
2025-06-23 12:02:24 -0700

    enrichment api

--- hooli-data-eng/hooli_data_eng/defs/batch_enrichment/resources.py
+++ hooli-data-eng/hooli_data_eng/defs/batch_enrichment/resources.py
@@ -9,7 +9,7 @@ from hooli_data_eng.defs.batch_enrichment.warehouse import MyWarehouse
 
 defs = Definitions(
     resources={
-        "api": EnrichmentAPI(),
+        "enrichment_api": EnrichmentAPI(),
         # place holder for a real warehouse with required connection config
         "warehouse": MyWarehouse(
             path=str(Path(__file__).parent.joinpath("raw_data.csv"))
