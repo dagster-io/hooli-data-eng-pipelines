60548d47c9536c69e632eb990f12437e282aff28
benpankow <ben@dagsterlabs.com>
2025-06-23 12:02:00 -0700

    bi import fix

--- hooli-data-eng/hooli_data_eng/defs/bi/resources.py
+++ hooli-data-eng/hooli_data_eng/defs/bi/resources.py
@@ -1,6 +1,6 @@
 from dagster import Definitions
 
-from hooli_data_eng.bi.powerbi_workspace import power_bi_workspace
+from hooli_data_eng.defs.bi.powerbi_workspace import power_bi_workspace
 
 defs = Definitions(
     resources={"power_bi": power_bi_workspace},
