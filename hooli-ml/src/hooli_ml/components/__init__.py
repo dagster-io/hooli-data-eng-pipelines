ababb486281faa16385c09da181c93d7caeedea0
Christian Minich <indyman402@gmail.com>
2025-09-30 16:52:05 -0400

    use component

--- hooli-ml/src/hooli_ml/components/__init__.py
+++ hooli-ml/src/hooli_ml/components/__init__.py
@@ -3,9 +3,11 @@ from .databricks_notebook_component import (
     NotebookTaskConfig,
     MultiNotebookJobConfig,
 )
+from dagster_databricks.components.databricks_asset_bundle.component import DatabricksAssetBundleComponent
 
 __all__ = [
     "DatabricksMultiNotebookJobComponent",
     "NotebookTaskConfig",
     "MultiNotebookJobConfig",
+    "DatabricksAssetBundleComponent",
 ]
