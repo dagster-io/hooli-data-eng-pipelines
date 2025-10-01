8da1fc819ab068f564582a7a179dd6e6c7fb5d9e
Christian Minich <indyman402@gmail.com>
2025-10-01 10:54:49 -0400

    remove custom component

--- hooli-ml/src/hooli_ml/components/__init__.py
+++ hooli-ml/src/hooli_ml/components/__init__.py
@@ -1,13 +1,5 @@
-from .databricks_notebook_component import (
-    DatabricksMultiNotebookJobComponent,
-    NotebookTaskConfig,
-    MultiNotebookJobConfig,
-)
 from dagster_databricks.components.databricks_asset_bundle.component import DatabricksAssetBundleComponent
 
 __all__ = [
-    "DatabricksMultiNotebookJobComponent",
-    "NotebookTaskConfig",
-    "MultiNotebookJobConfig",
     "DatabricksAssetBundleComponent",
 ]
