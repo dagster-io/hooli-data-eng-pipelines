6b7bf85dea58b3a0b84ad651eed9241a41047101
Christian Minich <indyman402@gmail.com>
2025-10-01 11:16:53 -0400

    remove more old files

--- hooli-ml/stashed_files/multi_notebook_job_component_gen/defs.py
+++
@@ -1,8 +0,0 @@
-"""
-Component definitions for the multi notebook job component.
-"""
-
-import dagster as dg
-
-# Load the component instance from the YAML configuration
-defs = dg.load_component_from_yaml(dg.file_relative_path(__file__, "defs.yaml"))
