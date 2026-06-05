e7c4b918909eac6404259135633670dfb8f185b6
Christian Minich <indyman402@gmail.com>
2026-06-05 13:27:23 -0400

    add a change to hooli-data-eng-pipelines

--- dbt_project/models/CLEANED/locations_cleaned.sql
+++ dbt_project/models/CLEANED/locations_cleaned.sql
@@ -7,6 +7,7 @@ from {{ source("raw_data", "locations") }}
  
 source_renamed as (
 
+    
     select l_user_id as user_id,
            l_street_address as street_address,
            l_state as state,
